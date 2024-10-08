4049898e90144bd769430fb4aebd5b2aa0c94a2e
ADMIN-PC\Admin <haubtm699@gmail.com>
2024-10-08 10:49:56 +0700

    2 commit

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ numbers = [1, 2, 3, 4];
 const squaredNumbers = numbers.map(num => num * num);
 
 console.log(squaredNumbers); // [1, 4, 9, 16]
+
+numbers = [1, 2, 3, 4];
+numbers.forEach(function (num) {
+  console.log(num * 2);
+});
+
+const evenNumbers = numbers.filter(function (num) {
+  return num % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+const sum = numbers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(sum);
+
+const found = numbers.find(function (num) {
+  return num > 3;
+});
+
+console.log(found);
+
+const hasEven = numbers.some(function (num) {
+  return num % 2 === 0;
+});
+
+console.log(hasEven);
